a957a1a2a13a74960134ce4529eaa77620fec6e2
Neil <neilwatts86@gmail.com>
2022-11-14 16:01:32 +0000

    started changes

--- src/main/java/OrderCalculator.java
+++ src/main/java/OrderCalculator.java
@@ -4,20 +4,63 @@ import java.util.List;
 
 public class OrderCalculator {
 
+//    public double getOrderTotal(ArrayList<String> booksInOrder) {
+//        List<ArrayList<String>> diverseBaskets = new ArrayList<>();
+//        for (int i = 0; i < booksInOrder.size(); i++)
+//        {
+//            diverseBaskets.add(new ArrayList<String>());
+//        }
+//        for (String book : booksInOrder)
+//        {
+//            for (ArrayList<String> diverseBasket : diverseBaskets)
+//            {
+//                if (!diverseBasket.contains(book))
+//                {
+//                    diverseBasket.add(book);
+//                    break;
+//                }
+//            }
+//        }
+//        double total = 0;
+//        for (ArrayList<String> diverseBasket : diverseBaskets)
+//            total += countDiverseBasket(diverseBasket);
+//
+//        return total;
+//
+//
+//    }
+
     public double getOrderTotal(ArrayList<String> booksInOrder) {
+
         List<ArrayList<String>> diverseBaskets = new ArrayList<>();
         for (int i = 0; i < booksInOrder.size(); i++)
         {
             diverseBaskets.add(new ArrayList<String>());
         }
+
+        int countOfNoneEmptyDiverseBaskets = 0;
+
         for (String book : booksInOrder)
         {
+            double priceIncrement = 8;
             for (ArrayList<String> diverseBasket : diverseBaskets)
             {
-                if (!diverseBasket.contains(book))
+
+                double priceIncrease = CalculatePriceIncrease(diverseBasket.size());
+                if (!diverseBasket.contains(book) && priceIncrease < priceIncrement )
                 {
-                    diverseBasket.add(book);
-                    break;
+                    priceIncrement = priceIncrease;
+//                    countOfNoneEmptyDiverseBaskets++;
+//                    diverseBasket.add(book);
+//                    break;
+                }
+                if (priceIncrement == 8)
+                {
+                    diverseBaskets.get(countOfNoneEmptyDiverseBaskets).add(book);
+                    countOfNoneEmptyDiverseBaskets++;
+                }
+                else{
+
                 }
             }
         }
@@ -27,6 +70,21 @@ public class OrderCalculator {
 
         return total;
 
+
+    }
+
+    private double CalculatePriceIncrease(int diverseBasketSize){
+
+        if (diverseBasketSize == 1)
+            return 7.2;
+        if (diverseBasketSize == 2)
+            return 6.4;
+        if (diverseBasketSize == 3)
+            return 4;
+        if (diverseBasketSize == 4)
+            return 4.4;
+
+        return 8.0;
 
     }
 
