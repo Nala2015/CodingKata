db4a0ae5607c83b2bde132a70f559c6d4766ff8f
Radivoj <stupar.raso@gmail.com>
2022-11-14 15:10:28 +0000

    Egde cases started

--- src/main/java/OrderCalculator.java
+++ src/main/java/OrderCalculator.java
@@ -1,16 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class OrderCalculator {
 
-    public double getOrderTotal(String[] booksInOrder) {
-        if(booksInOrder.length == 0) {
+    public double getOrderTotal(ArrayList<String> booksInOrder) {
+        List<ArrayList<String>> diverseBaskets = new ArrayList<>();
+        for (int i = 0; i < booksInOrder.size(); i++)
+        {
+            diverseBaskets.add(new ArrayList<String>());
+        }
+        for (String book : booksInOrder)
+        {
+            for (ArrayList<String> diverseBasket : diverseBaskets)
+            {
+                if (!diverseBasket.contains(book))
+                {
+                    diverseBasket.add(book);
+                    break;
+                }
+            }
+        }
+        double total = 0;
+        for (ArrayList<String> diverseBasket : diverseBaskets)
+            total += countDiverseBasket(diverseBasket);
+
+        return total;
+
+
+    }
+
+    public double countDiverseBasket(ArrayList<String> booksInOrder)
+    {
+        if(booksInOrder.size() == 0) {
             return 0.00;
         }
-        if((booksInOrder.length == 2) && (!booksInOrder[0].equals(booksInOrder[1]))) {
-            return 15.20;
-        }
-        if(booksInOrder.length == 2) {
-            return 16.00;
-        }
-        return 8.00;
+        else if(booksInOrder.size() == 1)
+            return 8.00;
+        else if (booksInOrder.size() == 2)
+            return 15.2;
+        else if (booksInOrder.size() == 3)
+            return 21.6;
+        else if (booksInOrder.size() == 4)
+            return 25.6;
+        else return 30;
     }
 
 }
--- src/test/java/PotterKata2Test.java
+++ src/test/java/PotterKata2Test.java
@@ -1,39 +1,138 @@
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PotterKata2Test {
+public class PotterKata2Test
+{
 
     @Test
-    public void anEmptyOrderReturnsZero() {
+    public void anEmptyOrderReturnsZero()
+    {
         OrderCalculator sut = new OrderCalculator();
-        String[] booksInOrder = {};
+        ArrayList<String> booksInOrder = new ArrayList<>();
         double orderAmount = sut.getOrderTotal(booksInOrder);
         assertEquals(orderAmount, 0);
     }
 
     @Test
-    public void oneBookInOrderReturnsUnitPrice() {
+    public void oneBookInOrderReturnsUnitPrice()
+    {
         OrderCalculator sut = new OrderCalculator();
-        String[] booksInOrder = {"One"};
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
         double orderAmount = sut.getOrderTotal(booksInOrder);
         assertEquals(orderAmount, 8);
     }
 
     @Test
-    public void twoDifferentBooksReturnsFivePercentOff() {
+    public void twoDifferentBooksReturnsFivePercentOff()
+    {
         OrderCalculator sut = new OrderCalculator();
-        String[] booksInOrder = {"One", "Two"};
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("Two");
         double orderAmount = sut.getOrderTotal(booksInOrder);
         assertEquals(orderAmount, 15.20);
     }
 
     @Test
-    public void twoOfTheSameBookReturnsUnitPriceTimesTwo() {
+    public void twoOfTheSameBookReturnsUnitPriceTimesTwo()
+    {
         OrderCalculator sut = new OrderCalculator();
-        String[] booksInOrder = {"One", "One"};
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("One");
         double orderAmount = sut.getOrderTotal(booksInOrder);
         assertEquals(orderAmount, 16.00);
     }
+
+    @Test
+    public void fiveDifferentBooksReturn25PercentOff()
+    {
+        OrderCalculator sut = new OrderCalculator();
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("Two");
+        booksInOrder.add("Three");
+        booksInOrder.add("Four");
+        booksInOrder.add("Five");
+        double orderAmount = sut.getOrderTotal(booksInOrder);
+        assertEquals(orderAmount, 30.00);
+    }
+
+    @Test
+    public void threeDifferentBooksReturn10PercentOff()
+    {
+        OrderCalculator sut = new OrderCalculator();
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("Two");
+        booksInOrder.add("Three");
+        double orderAmount = sut.getOrderTotal(booksInOrder);
+        assertEquals(orderAmount, 21.60);
+    }
+
+    @Test
+    public void fourDifferentBooksReturn20PercentOff()
+    {
+        OrderCalculator sut = new OrderCalculator();
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("Two");
+        booksInOrder.add("Three");
+        booksInOrder.add("Four");
+        double orderAmount = sut.getOrderTotal(booksInOrder);
+        assertEquals(orderAmount, 25.60);
+    }
+
+    @Test
+    public void orderingOneUniqueBookAndTwoDuplicateBooksReturnsDiscountOf5Percent()
+    {
+        OrderCalculator sut = new OrderCalculator();
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("One");
+        booksInOrder.add("Three");
+        double orderAmount = sut.getOrderTotal(booksInOrder);
+        assertEquals(orderAmount, 23.20);
+    }
+
+    @Test
+    public void orderingTwoSetsOfTwoAndGettingTwoTimes5PercentDiscount()
+    {
+        OrderCalculator sut = new OrderCalculator();
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("One");
+        booksInOrder.add("Three");
+        booksInOrder.add("Three");
+        double orderAmount = sut.getOrderTotal(booksInOrder);
+        assertEquals(orderAmount, 30.4);
+    }
+
+    @Test
+    public void testingThreeTimesTwoCopiesPlusTwoUniqueOnes()
+    {
+        OrderCalculator sut = new OrderCalculator();
+        ArrayList<String> booksInOrder = new ArrayList<>();
+        booksInOrder.add("One");
+        booksInOrder.add("One");
+        booksInOrder.add("Second");
+        booksInOrder.add("Second");
+        booksInOrder.add("Three");
+        booksInOrder.add("Three");
+        booksInOrder.add("Four");
+        booksInOrder.add("Five");
+        double orderAmount = sut.getOrderTotal(booksInOrder);
+
+        assertEquals(51.20,orderAmount);
+    }
+
+
+
+
+
 }
